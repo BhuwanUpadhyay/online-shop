b3e1d031e46251ce2e6caf71cb71b28df1394095
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-08-04 11:43:09 +0545

    chore(wip): modules

--- specifications/ci-prepare.sh
+++ specifications/ci-prepare.sh
@@ -19,24 +19,31 @@ echo "Current script directory: $SCRIPT_DIR"
 echo "New semantic version: $NEW_VERSION"
 
 for module in \
-  "inventory" \
+  "shopping-cart" \
   ; do
     echo "PACKAGING MODULE: ${module}"
     for feature in \
         "spring-boot" \
         "java-client" \
-        ; do
-        echo "PACKAGING FEATURE: ${feature}"
-        cd "$SCRIPT_DIR"/dist/${module}/${feature}
-        mvn_install
-        cd "$SCRIPT_DIR"
-    done
-    for feature in \
         "typescript-client" \
         ; do
+
         echo "PACKAGING FEATURE: ${feature}"
+
         cd "$SCRIPT_DIR"/dist/${module}/${feature}
-        npm_link
+
+        case $feature in
+          spring-boot)
+            mvn_install
+            ;;
+          java-client)
+            mvn_install
+            ;;
+          typescript-client)
+#            npm_link
+            ;;
+        esac
+
         cd "$SCRIPT_DIR"
     done
 done
