70aeeaad02f52f05c9571f2785c7360cf2640395
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-08-12 14:47:39 +0545

    chore(wip): modules

--- specifications/ci-prepare.sh
+++ specifications/ci-prepare.sh
@@ -4,6 +4,22 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+mvn_deploy() {
+  curl https://gist.githubusercontent.com/BhuwanUpadhyay/b626684dcbd710bc27756be72b7128a6/raw/5dfced7406ea9b51f78391e6faf49746c44db152/cloudsmith-settings.xml \
+    --output cloudsmith-settings.xml
+
+  sed -i "s|</project>|<distributionManagement><snapshotRepository><id>cloudsmith</id><url>https://maven.cloudsmith.io/bhuwan-prasad-upadhyay/maven/</url></snapshotRepository><repository><id>cloudsmith</id><url>https://maven.cloudsmith.io/bhuwan-prasad-upadhyay/maven/</url></repository></distributionManagement></project>|" pom.xml
+
+  mvn source:jar-no-fork \
+      deploy
+}
+
+npm_publish() {
+  echo "//npm.cloudsmith.io/bhuwan-prasad-upadhyay/maven/:_authToken=${NPM_AUTH_TOKEN}" > $HOME/.npmrc
+  npm config set '@rtms:registry' https://npm.cloudsmith.io/bhuwan-prasad-upadhyay/maven/
+  npm i && npm run build && cd dist/ && npm pack && npm publish
+}
+
 mvn_install() {
   mvn \
       source:jar-no-fork \
@@ -19,7 +35,7 @@ echo "Current script directory: $SCRIPT_DIR"
 echo "New semantic version: $NEW_VERSION"
 
 for module in \
-  "shopping-cart" \
+  "cart" \
   "delivery" \
   "inventory" \
   "history" \
