4b970536cee973bc36f405b26a34d9fce04ad657
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-08-12 09:03:50 +0545

    chore(wip): modules

--- microservices/shared-kernel/src/main/java/io/github/bhuwanupadhyay/ddd/DomainEvent.java
+++ microservices/shared-kernel/src/main/java/io/github/bhuwanupadhyay/ddd/DomainEvent.java
@@ -1,19 +1,18 @@
 package io.github.bhuwanupadhyay.ddd;
 
-import java.util.Objects;
 import java.util.UUID;
 
 public abstract class DomainEvent {
 
-  private final String eventId = UUID.randomUUID().toString();
+    private final String eventId = UUID.randomUUID().toString();
 
-  private final String eventClassName = getClass().getName();
+    private final String eventClassName = getClass().getName();
 
-  public String getEventId() {
-    return eventId;
-  }
+    public String getEventId() {
+        return eventId;
+    }
 
-  public String getEventClassName() {
-    return eventClassName;
-  }
+    public String getEventClassName() {
+        return eventClassName;
+    }
 }
